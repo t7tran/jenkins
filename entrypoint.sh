aefec8fda946a80e8369f6acc3b02f18be293c3e
Tien Tran <tien@transfamily.info>
2023-08-03 11:49:27 +1000

    Remove update blocking

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ set -e
 
 cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-grep -q 'updates.jenkins.io' /etc/hosts && echo || bash -c "echo '127.0.0.1 updates.jenkins.io' >> /etc/hosts"
-
 if [[ -z "$@" ]]; then
 	gosu jenkins /sbin/tini -- /usr/local/bin/jenkins.sh
 else
